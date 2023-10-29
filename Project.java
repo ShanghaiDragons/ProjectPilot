62c31f32bdd23a039a97fc29384f82d6d4cbb937
Theodore Villalva <theodorevillalva@gmail.com>
2023-10-29 19:17:56 -0400

    removed threadComments in Project.java

--- Project.java
+++ Project.java
@@ -135,17 +135,6 @@ public class Project {
     }
 
     /**
-     * 
-     * @param user
-     * @param project
-     * @param comment
-     * @return
-     */
-    private boolean threadComment(User user, Project project, String comment) {
-        return false;
-    }
-
-    /**
      * Gets the name of the project
      * @author Chris
      * @return the project name (this.projectName)
