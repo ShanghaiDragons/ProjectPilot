7b4a33d3d17faa50295a066ae74bc446ee13c171
Duayne <duayne@email.sc.edu>
2023-11-09 01:14:18 -0500

    added to DataLoader/Writer Test class

--- Testing/DataLoaderTest.java
+++ Testing/DataLoaderTest.java
@@ -3,57 +3,73 @@ package Testing;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.time.LocalDate;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
 import sourceCode.*;
 
 class DataLoaderTest {
 	private UserList users = UserList.getInstance();
+	private ProjectList projects = ProjectList.getInstance();
+	private DataLoader dataLoader = DataLoader.getInstance();
 	private ArrayList<User> userList = users.getUsers();
+	private ArrayList<Project> projectList = projects.getProjects();
+	private ArrayList<Column> columnlist;
+    private ArrayList<Comment> commentlist;
+	private ArrayList<Task> tasks;
+	private DataWriter dataWriter = new DataWriter();
 	
 	@BeforeEach
 	public void setup() {
 		userList.clear();
+		projectList.clear();
+		columnlist = new ArrayList<Column>();
+		commentlist = new ArrayList<Comment>();
+		tasks = new ArrayList<Task>();
 		userList.add(new User("Amy", "Smith", "aSmith24", "p", false, false, false, false));
 		userList.add(new User("Bobby", "Smith", "bSmith24", "p", false, false, false, false));
-		DataWriter dw = new DataWriter();
-		dw.saveUsers();
+		projectList.add(new Project("Project1", LocalDate.parse("2023-11-07"), LocalDate.parse("2023-11-08"), userList, columnlist, commentlist));
+		tasks.add(new Task("newTask", userList.get(0), 1, "null", "desc", commentlist));
+		columnlist.add(new Column("", "null", tasks, commentlist));
+		dataWriter.saveUsers();
+		dataWriter.saveProjects();
 	}
 	
-	@AfterEach
+	@After
 	public void tearDown() {
-		UserList.getInstance().getUsers().clear();
-		DataWriter dw = new DataWriter();
-		dw.saveUsers();
-		}
+		userList.clear();
+		projectList.clear();
+		dataWriter.saveUsers();
+		dataWriter.saveProjects();
+	}
 	
-	
-		
 	@Test
 	void testGetUsersSize() {
-		userList = UserList.getInstance().getUsers();
-		assertEquals(2, userList.size());
+		assertEquals(2, dataLoader.getUsers().size());
 	}
 
 	@Test
 	void testGetUsersSizeZero() {
-		UserList.getInstance().getUsers().clear();
-		DataWriter dw = new DataWriter();
-		dw.saveUsers();
-		assertEquals(0, userList.size());
-	}
-	
-	@Test
-	void testGetUserName() {
-		assertEquals("aSmith24", UserList.getInstance().getUser("aSmith24").getUserName());
+		userList.clear();
+		dataWriter.saveUsers();
+		assertEquals(0, dataLoader.getUsers().size());
 	}
 
 	@Test
-	void testGetUserFirstName() {
-		userList = UserList.getInstance().getUsers();
-		assertEquals("aSmith24", userList.get(0).getUserName());
+	void testGetProjectsSize() {
+		assertEquals(1, dataLoader.getProjects().size());
 	}
+
+	@Test
+	void testGetProjectsSizeZero() {
+		projectList.clear();
+		dataWriter.saveProjects();
+		assertEquals(0, dataLoader.getProjects().size());
+	}
+	
 }
+
+// add a check for no file name given
+// saving an empty string
