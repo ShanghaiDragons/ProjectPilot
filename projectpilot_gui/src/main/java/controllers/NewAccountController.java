3d880c6c4c01dd48d100106701fa595794e9adad
Theodore Villalva <theodorevillalva@gmail.com>
2023-12-06 10:11:41 -0500

    revised addaccount in ppf so that it has password validation, also added pop up messages for failed password/successful login

--- projectpilot_gui/src/main/java/controllers/NewAccountController.java
+++ projectpilot_gui/src/main/java/controllers/NewAccountController.java
@@ -6,10 +6,12 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.*;
@@ -49,16 +51,28 @@ public class NewAccountController implements Initializable{
     private void switchToHome() throws IOException {
         if (ppf.createAccount(txt_fName.getText(), txt_lName.getText(), txt_username.getText(), txt_password.getText())){
             ppf.saveUsers();
+            showAlert("Success!", "You have successfully created a new account!");
             App.setRoot("home");
         } else {
-            txt_fName.clear();
-            txt_lName.clear();
-            txt_username.clear();
-            txt_password.clear();
-            lbl_errorMessage.setVisible(true);
+            showERROR("Error!", "Password must be:\n atleast 6 characters\ncontain atleast one letter [a-Z]\ncontain atleast one number [0-9]");
         }
         
         
+    }
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void showERROR(String title, String content) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
     @FXML
