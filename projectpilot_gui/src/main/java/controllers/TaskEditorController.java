7db84a1b87d077ad8116efb560070164fc163db8
ChrisTaks35 <christaks35@gmail.com>
2023-12-06 21:34:08 -0500

    TeskEditorController fixes

--- projectpilot_gui/src/main/java/controllers/TaskEditorController.java
+++ projectpilot_gui/src/main/java/controllers/TaskEditorController.java
@@ -6,17 +6,12 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
@@ -28,45 +23,33 @@ import model.*;
 public class TaskEditorController implements Initializable{
     private ProjectPilotFacade ppf;
     private Task currentTask;
+    private static final int PRIORITY_NUM = 3;
     @FXML
     private Button btn_backToHome;
-
     @FXML
     private Button btn_saveChanges;
-
     @FXML
     private Label lbl_assigneeSelection;
-
     @FXML
     private Label lbl_comment1;
-
     @FXML
     private Label lbl_prioritySelection;
-
     @FXML
-    private ListView<String> list_assignee;
-
+    private ListView<User> list_assignee;
     @FXML
     private ListView<Integer> list_priority;
-
     @FXML
     private TitledPane titledPane_assignee;
-
     @FXML
     private TitledPane titledPane_priority;
-
     @FXML
     private TextField txt_add_comment;
-
     @FXML
     private TextField txt_taskTitle;
-
     @FXML
     private TextField txt_task_description;
     @FXML
     private ImageView background_pic;
-    private ObservableList<Integer> priorities = FXCollections.observableArrayList();
-    private ObservableList<String> users = FXCollections.observableArrayList();
 
     /**
      * Initializes the facade to populate the data in the scene
@@ -82,12 +65,8 @@ public class TaskEditorController implements Initializable{
         txt_task_description.setText(currentTask.getDescription());
         lbl_prioritySelection.setText(String.valueOf(currentTask.getPriority()));
         lbl_assigneeSelection.setText(currentTask.getAssignee().getUserName());
-        for ( int i = 1; i < 4; i++ )
-            priorities.add(i);
-        for (User team : ppf.getUsers() )
-            users.add(team.getFirstName() + " " + team.getLastName());
-        list_priority.setItems(priorities);
-        list_assignee.setItems(users);
+        setChangePriority();
+        setChangeAssignee();
 
         list_priority.setOnMouseClicked(event -> {
             try {
@@ -109,26 +88,26 @@ public class TaskEditorController implements Initializable{
         background_pic.setImage(background);
     }
 
+    /**
+     * Saves the edited task to ProjectPilotFacade's currentTask
+     * @param event
+     * @throws IOException
+     */
     @FXML
     private void saveChanges(ActionEvent event) throws IOException {
-        User assignee = null;
-        for (User user : ppf.getUsers())
-            if (user.getUserName().equals(lbl_assigneeSelection.getText()))
-                assignee = user;
         currentTask.setName(txt_taskTitle.getText());
         currentTask.setDescription(txt_task_description.getText());
-        currentTask.setPriority(Integer.parseInt(lbl_prioritySelection.getText()));
-        currentTask.setAssignee(assignee);
+        ppf.setCurrentTask(currentTask);
+
         switchToHome(event);
     }
 
     /**
      * 
+     * @author duayne
+     * @param event mouse click
+     * @throws IOException
      */
-    private void displayTask() {
-
-    }
-
     @FXML
     private void expandLists(MouseEvent event) throws IOException {
         if (titledPane_priority.isExpanded())
@@ -139,29 +118,61 @@ public class TaskEditorController implements Initializable{
         }
     }
 
+    /**
+     * What happens when a priority is selected
+     * @author ctaks
+     * @param event mouse click
+     * @throws IOException
+     */
     @FXML
     private void priorityItemSelected(MouseEvent event) throws IOException {
-        String priority = list_priority.getSelectionModel().getSelectedItem().toString();
-        lbl_prioritySelection.setText(priority);
+        int selectedPriority = list_priority.getSelectionModel().getSelectedItem();
+        lbl_prioritySelection.setText(String.valueOf(selectedPriority));
+        currentTask.setPriority(selectedPriority);
     }
 
+    /**
+     * What happens when an assignee is selected
+     * @author ctaks
+     * @param event mouse click
+     * @throws IOException
+     */
     @FXML
     private void assigneeItemSelected(MouseEvent event) throws IOException {
-        String assignee = "";
-        for (User user : ppf.getUsers())
-            if (list_assignee.getSelectionModel().getSelectedItem().toString().equals(user.getFirstName() + " " + user.getLastName()))
-                assignee = user.getUserName();
+        User selectedUser = list_assignee.getSelectionModel().getSelectedItem();
+        currentTask.setAssignee(selectedUser);
+        lbl_assigneeSelection.setText(currentTask.getAssignee().getUserName());
+    }
+
+    /**
+     * Sets up the priority dropdown
+     * @author ctaks
+     */
+    private void setChangePriority() {
+        ObservableList<Integer> priorityList = FXCollections.observableArrayList();
+
+        for (int i = 1; i <= PRIORITY_NUM; i++) {
+            Integer wrappedI = i;
+            priorityList.add(wrappedI);
+        }
+        list_priority.setItems(priorityList);
+    }
+
+     /**
+     * Sets up the assignee dropdown
+     * @author ctaks
+     */
+    private void setChangeAssignee() {
+        ObservableList<User> userList = FXCollections.observableArrayList();
         
-        lbl_assigneeSelection.setText(assignee);
+        for (User user : ppf.getUsers()) {
+            userList.add(user);
+        }
+        list_assignee.setItems(userList);
     }
 
     @FXML
     void addCommentToTask(ActionEvent event) throws IOException{
-
-    }
-
-    @FXML
-    void setAssignee(ActionEvent event) throws IOException{
 
     }
 
